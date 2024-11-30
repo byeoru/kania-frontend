2364f59af3e8b8c7eb35f5567799b3feb7d1ca46
byeoru <byeoru215@gmail.com>
2024-11-30 23:20:13 +0900

    Router 구현

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+  import Home from './routes/Home.svelte';
+  import type { Component } from 'svelte';
+  import NotFound from './routes/NotFound.svelte';
+
+  // 현재 경로 상태
+  let currentRoute: string = window.location.pathname;
+
+  // // 경로와 컴포넌트 매핑
+  const routes: Record<string,  Component> = {
+    '/': Home
+  };
+
+  // URL 변경 감지 함수
+  const handleRouteChange = (): void => {
+    currentRoute = window.location.pathname;
+  };
+
+  // popstate 이벤트로 뒤로가기/앞으로가기 감지
+  window.addEventListener('popstate', handleRouteChange);
+
+  // 네비게이션 함수
+  const navigate = (path: string): void => {
+    if (path !== currentRoute) {
+      history.pushState(null, '', path);
+      handleRouteChange();
+    }
+  };
+</script>
+
+<!-- <nav>
+  <a href="/" on:click|preventDefault={() => navigate('/')}>Home</a>
+</nav> -->
+
+<main>
+  {#if routes[currentRoute]}
+    <svelte:component this={routes[currentRoute]} />
+  {:else}
+    <NotFound />
+  {/if}
+</main>
---
+++ src/routes/Home.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  const name:string = 'Home';
+</script>
+
+<div>{name}</div>
